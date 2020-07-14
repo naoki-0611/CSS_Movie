f26047d5b02fabf001653ab00e89dfc9d8706031
Taiga <taiga248@gmail.com>
2020-07-14 16:49:53 +0900

    Add: fifth section animation

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const firstSec = document.getElementById("first-section");
 const secondSec = document.getElementById("second-section");
 const thirdSec = document.getElementById("third-section");
 const fourthSec = document.getElementById("fourth-section");
+const fifthSec = document.getElementById("fifth-section");
 
-// TODO: 誤差が多い 原因改名と修正
 const processTime = 250;
 const firstAnimationEndTime = 5500 + processTime;
-const secondAnimationEndTime = firstAnimationEndTime;
-const thirdAnimationEndTime = secondAnimationEndTime - 2000;
-// const fourthAnimationEndTime = 3000 + processTime;
+const secondAnimationEndTime = 4500 + processTime;
+const thirdAnimationEndTime = 2000 + processTime;
+const fourthAnimationEndTime = 4000 + processTime;
+const fifthAnimationEndTime = 1500 + processTime;
 
 const displayNone = section => {
   section.style.display = "none";
@@ -23,5 +24,8 @@ Promise.resolve()
   .then(() => displayNone(secondSec))
   .then(() => wait(thirdAnimationEndTime))
   .then(() => displayNone(thirdSec))
-  // .then(() => displayNone(fourthSec))
+  .then(() => wait(fourthAnimationEndTime))
+  .then(() => displayNone(fourthSec))
+  .then(() => wait(fifthAnimationEndTime))
+  .then(() => displayNone(fifthSec))
   .catch(error => console.log(error));
